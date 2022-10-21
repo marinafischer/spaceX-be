4edf2412f310a5d78ad504c1016638658ca4dbcf
marinafischer <marinamirandafischer@gmail.com>
2022-10-21 15:46:55 -0300

    feat: connect with route

--- src/routes/launchesRoute.js
+++ src/routes/launchesRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { getNextLaunch } = require('../controllers/launchesController');
 
 const router = express.Router();
 
-router.get('/next', (req, res) => { res.status(200).send('oiii'); });
+router.get('/next', getNextLaunch);
 
 module.exports = router;
